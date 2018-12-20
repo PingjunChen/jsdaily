8d8dde9b02ac54217d0d089d4a8de00bad661665
pingjun <chenpingjun1990@ufl.edu>
2018-12-21 07:13:55 +0800

    add test for jsdaily

---
+++ test/test-daily.js
@@ -0,0 +1,8 @@
+const daily = require("../index");
+let color = daily.getRandomColor();
+console.log("Color is: " + color);
+let us_str = "United 	States ";
+let tiny_str = daily.removeStringSpace(us_str);
+console.log(tiny_str);
+
+
